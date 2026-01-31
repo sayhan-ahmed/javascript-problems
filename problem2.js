eadd50638aac689e4d9fbdc3dd1c512f7a600ffb
Sayhan Ahmed <sayanahmed228@gmail.com>
2026-02-01 00:03:47 +0600

    feat: solve Problem 2 with more 5 different methods

--- problem2.js
+++ problem2.js
@@ -23,3 +23,47 @@ function isEven(num) {
 }
 const number3 = isEven(9);
 
+// Method 4
+
+function checkEvenOdd(num) {
+  return num % 2 === 0 ? "The 4th number is Even" : "The 4th number is Odd";
+}
+console.log(checkEvenOdd(10));
+
+// Method 5 : Bitwise AND
+
+const number5 = 11;
+
+if (number5 & 1) {
+  console.log("The 5th number is Odd");
+} else {
+  console.log("The 5th number is Even");
+}
+
+// or
+
+function checkEvenOdd2(num) {
+  return (num & 1) === 0 ? "The 5th number is Even" : "The 5th number is Odd";
+}
+console.log(checkEvenOdd2(11));
+
+// Method 6 : Object Mapping
+
+function checkEvenOdd(num) {
+  const result = { 0: "The 6th number is Even", 1: "The 6th number is Even" };
+  return result[num % 2];
+}
+console.log(checkEvenOdd(12));
+
+// Method 7 : Array Indexing
+
+function checkEvenOdd3(num) {
+  return ["The 7th number is Even", "The 7th number is Odd"][Math.abs(num % 2)];
+}
+console.log(checkEvenOdd3(13));
+
+// Method 8 : Logical Falsy Shorthand
+
+const checkEvenOdd4 = (num) =>
+  num % 2 ? "The 8th number is Odd" : "The 8th number is Even";
+console.log(checkEvenOdd4(14));
