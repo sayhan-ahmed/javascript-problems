a00928ae509b7d3e2e9e092c8ac28a7eabe90de7
Sayhan Ahmed <sayanahmed228@gmail.com>
2026-02-04 23:53:49 +0600

    solve: Problem 3 with 2 more methods

--- problem3.js
+++ problem3.js
@@ -33,3 +33,18 @@ function isBig2(array) {
 }
 
 console.log(isBig2([1, 4, 28, 9, 10, 39, 49]));
+
+// === Method 3 === //
+const numbers3 = [1, 5, 3, 9, 2, 7, 32, 34, 8, 2, 4];
+const largest = Math.max.apply(null, numbers3);
+
+console.log(largest);
+
+// === Method 4 === //
+function getMaxWithReduce(arr) {
+  return arr.reduce((max, current) => {
+    return current > max ? current : max;
+  }, arr[0]);
+}
+
+console.log(getMaxWithReduce([4, 2, 5, 3, 8, 1, 4]));
