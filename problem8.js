06f1ca372ed501f8ab5d109646bad4cff2f8be60
Sayhan Ahmed <sayanahmed228@gmail.com>
2026-02-05 22:27:49 +0600

    solve: Problem 8 with 2 more different methods

--- problem8.js
+++ problem8.js
@@ -6,6 +6,7 @@
 // - Input: [-2, 5, 0, -8, 12]
 // - Output: [5, 12]
 
+// === Method 1 === //
 const numbers = [-2, 5, 0, -8, 12];
 
 let positiveNum = [];
@@ -15,3 +16,19 @@ for (const num of numbers) {
   }
 }
 console.log(positiveNum);
+
+// === Method 2 === //
+const numbers2 = [-2, 5, 0, -8, 12, 47, 13, -13, 3];
+
+const positiveNumbers = numbers2.filter((num) => num > 0);
+
+console.log(positiveNumbers);
+
+// === Method 3 === //
+const numbers3 = [-21, 52, 0, -8, 12, 84, -1];
+let positiveNum3 = [];
+
+numbers3.forEach((num) => {
+  if (num > 0) positiveNum3.push(num);
+});
+console.log(positiveNum3);
